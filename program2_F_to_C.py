3c5f606b71005ed8ffc16bd21ba2df95a7194a8d
Evan G <80179007+evangohary@users.noreply.github.com>
2022-03-07 12:37:50 -0600

    Add files via upload

---
+++ program2_F_to_C.py
@@ -0,0 +1,14 @@
+#Evan G
+#formula covert celsiu to f: F = 9/5c+32
+
+#python equivilent:  f = 9/5 * c  + 32
+
+#python equivilent:  fahrenheit_temp = 9/5 * celsius_temp + 32
+
+celcius_temp = float(input('Please enter the temperature in Celsius: '))
+
+fahrenheit_temp = 9/5 * celcius_temp + 32
+
+print(format(fahrenheit_temp,',.1f'), "Degrees Fahrenheit")
+
+print("This program was written by evan g")
